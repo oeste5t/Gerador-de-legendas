1e021424987eeffbd7c384d1a0c8c546a19a520b
oeste5t <perigoreal00@gmail.com>
2026-05-02 23:40:29 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'captionpro-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './logo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response; // Retorna do cache se tiver
+        }
+        return fetch(event.request); // Senão busca da internet
+      })
+  );
+});
